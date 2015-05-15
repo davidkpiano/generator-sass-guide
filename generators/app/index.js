0ed504c289ed0983bf62a7f82b3129ae7c4b251a
David Khourshid <davidkpiano@gmail.com>
2015-05-15 07:55:20 -0400

    Adding scaffolding for SCSS

--- generators/app/index.js
+++ generators/app/index.js
@@ -10,11 +10,21 @@ var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_ag
     property = _x2,
     receiver = _x3; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; }
 
 var _yeomanGenerator = require('yeoman-generator');
+
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
 
 var SassGuideGenerator = (function (_Base) {
     function SassGuideGenerator() {
@@ -34,7 +44,7 @@ var SassGuideGenerator = (function (_Base) {
         get: function () {
             return {
                 appName: function appName() {
-                    var _this3 = this;
+                    var _this4 = this;
 
                     var done = this.async();
                     var prompt = [{
@@ -46,9 +56,25 @@ var SassGuideGenerator = (function (_Base) {
                     this.prompt(prompt, function (_ref) {
                         var appName = _ref.appName;
 
-                        _this3.options.appName = appName;
+                        _this4.options.appName = appName;
                         done();
                     });
+                }
+            };
+        }
+    }, {
+        key: 'writing',
+        get: function () {
+            return {
+                app: function app() {
+                    this.mkdir('sass');
+                    this.mkdir('sass/base');
+                    this.mkdir('sass/components');
+                    this.mkdir('sass/layout');
+                    this.mkdir('sass/pages');
+                    this.mkdir('sass/themes');
+                    this.mkdir('sass/utils');
+                    this.mkdir('sass/vendors');
                 }
             };
         }
