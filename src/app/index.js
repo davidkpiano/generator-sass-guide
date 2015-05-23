867422dfb3c11eda8c2400d57c4ce46253d40281
David Khourshid <davidkpiano@gmail.com>
2015-05-23 16:26:46 -0400

    Adding src directory (ES6)

---
+++ src/app/index.js
@@ -0,0 +1,44 @@
+import { Base } from 'yeoman-generator';
+import fs from 'fs';
+import path from 'path';
+
+// Paths
+const vendorPath = path.join(__dirname, '../../vendor');
+
+const paths = {
+  sass: path.join(vendorPath, 'sass-boilerplate/stylesheets') 
+};
+
+export default class SassGuideGenerator extends Base {
+  constructor(...args) {
+    super(...args);
+  }
+
+  get prompting() {
+    return {
+      appName() {
+        let done = this.async();
+        let prompt = [
+          {
+            type: 'input',
+            name: 'appName',
+            message: 'What is your app called?'
+          }
+        ];
+
+        this.prompt(prompt, ({ appName }) => {
+          this.options.appName = appName;
+          done();
+        });
+      }
+    }
+  }
+
+  get writing() {
+    return {
+      app() {
+        this.directory(paths.sass, 'sass');
+      }
+    }
+  }
+}
